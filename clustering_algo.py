f62612e1d9af76ce1c0212b8fb4dd60587764717
isaacambrogetti <isaac.ambrogetti@icloud.com>
2024-10-10 16:44:43 +0200

    test clusters

--- clustering_algo.py
+++ clustering_algo.py
@@ -1,30 +1,47 @@
-
-import numpy as np
 import pyvista as pv
+import numpy as np
 from sklearn.cluster import DBSCAN
-
+import matplotlib.pyplot as plt
 
 # Charger le fichier PLY
-pcd_pv = pv.read('0905_pl3_pot_pc.ply')
+ply_file = 'test_pc_cluster.ply'
+pcd_pv = pv.read(ply_file)
 points = np.array(pcd_pv.points)
 
-
-# Appliquer DBSCAN
-dbscan = DBSCAN(eps=0.03, min_samples=3).fit(points)  # Ajuste 'eps' selon tes données
+# Appliquer DBSCAN avec les param'e8tres ajust'e9s
+dbscan = DBSCAN(eps=0.75, min_samples=20).fit(points)
 labels = dbscan.labels_
 
+# Récupérer les valeurs uniques des clusters
+unique_labels = np.unique(labels)
 
-# Visualiser les clusters
+# Afficher les différentes valeurs
+print("Valeurs des clusters (éiquettes):", unique_labels)
+
+# Compter combien de points appartiennent à chaque cluster
+clusters={}
+for label in unique_labels:
+    count = np.sum(labels == label)
+    clusters[label] = count
+
+clusters_sorted=dict(sorted(clusters.items(), key=lambda item: item[1], reverse=True))
+biggest_clusters = list(clusters_sorted.keys())[:2]
+
+
+# Ajouter les labels au nuage de points et visualiser les clusters
 pcd_pv['labels'] = labels
-pcd_pv.plot(scalars='labels')
+pcd_pv.plot(scalars='labels', render_points_as_spheres=True, point_size=5)
 
 
 # Suppression du pot en filtrant le cluster qui lui correspond
-pot_cluster = 0  # Définir le cluster correspondant au pot
-mask = labels != pot_cluster
+plant_clusters = biggest_clusters  # Définir le cluster correspondant à la plante
+mask = labels in plant_clusters
 filtered_points = points[mask]
 
 
 # Sauvegarder les points filtrés
 filtered_pcd = pv.PolyData(filtered_points)
 filtered_pcd.save('filtered_plants.ply')
+
+
+# essayer d'ordonner les clusters par taille et de filtrer et garder les deux plus grands
